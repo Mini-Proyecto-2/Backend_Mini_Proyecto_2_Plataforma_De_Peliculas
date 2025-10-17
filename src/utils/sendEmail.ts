65ffc268860723a896eb7475cd7d8e79688a35e2
Le06arcia <132424081+Le06arcia@users.noreply.github.com>
2025-10-17 18:14:56 -0500

    Develop (#4)
    * feat: implement user authentication and database connection setup
    
    * feat: add bcrypt and nodemailer for authentication and email functionality; update MongoDB URI variable
    
    * feat: add cookie-parser for cookie handling; implement authentication middleware and update auth routes
    
    * fix: update reset password URL and email subject for consistency with Film Unity branding
    
    * fix: update deleteProfile function to destructure password from req.body.user
    
    ---------
    
    Co-authored-by: DavidReyesCuartas <david12033.dr@gmail.com>

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,57 @@
+/**
+ * @file utils/sendEmail.js
+ * @description Utility to send emails using Nodemailer.
+ */
+
+const nodemailer = require("nodemailer");
+
+/**
+ * Sends an email using the Nodemailer configuration.
+ *
+ * @async
+ * @function sendEmail
+ * @param {Object} options - Email options.
+ * @param {string} options.to - Recipient's email address.
+ * @param {string} options.subject - Email subject.
+ * @param {string} [options.text] - Plain text content of the email.
+ * @param {string} [options.html] - HTML content of the email (recommended for better formatting).
+ * @returns {Promise<void>} Promise that resolves when the email is sent successfully.
+ *
+ * @throws {Error} If an error occurs during email sending.
+ *
+ * @example
+ * await sendEmail({
+ *   to: "user@example.com",
+ *   subject: "Welcome to Film Unity 🎉",
+ *   html: "<h1>Thank you for signing up for Film Unity</h1>"
+ * });
+ */
+
+interface EmailOptions {
+    to: string;
+    subject: string;
+    text?: string;
+    html?: string;
+}
+
+const sendEmail = async (options: EmailOptions): Promise<void> => {
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: process.env.EMAIL_USER as string,
+            pass: process.env.EMAIL_PASS as string
+        }
+    });
+
+    const mailOptions = {
+        from: `"Film Unity 👻" <${process.env.EMAIL_USER}>`,
+        to: options.to,
+        subject: options.subject,
+        text: options.text || "", // opcional
+        html: options.html || "", // 👈 importante para que se vea el contenido HTML
+    };
+
+    await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
